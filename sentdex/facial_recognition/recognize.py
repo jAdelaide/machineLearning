511ed7d7949aee878f4fd9795f5f53e9c4008eda
Ubuntu <Jordan@jordo.pxztmidijvgepngrx4ylhqcf4b.bx.internal.cloudapp.net>
2021-01-20 19:06:55 +0000

    Facial recognition script

--- sentdex/facial_recognition/recognize.py
+++ sentdex/facial_recognition/recognize.py
@@ -40,4 +40,36 @@ for filename in os.listdir(UNKNOWN_FACES_DIR):
 
         # Check for matches
     for face_encoding, face_location in zip(encodings, locations):
-        results = face_recognition.compare_faces(known_faces, face_encoding)
+        results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
+        match = None
+        if True in results:
+            match = known_names[results.index(True)]
+            print(f"Match found: {match}")
+
+                # Show image with a box around the identified face
+            top_left = (face_location[3], face_location[0])
+            bottom_right = (face_location[1], face_location[2])
+                
+                # Make the box around the face
+            color = [0, 255, 0]
+            cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
+
+                # Make a text box for the name
+            top_left = (face_location[3], face_location[2])
+            bottom_right = (face_location[1], face_location[2]+22)
+            cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
+            cv2.putText(image, 
+                        match, 
+                        (face_location[3]+10, face_location[2]+15), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 
+                        0.5, 
+                        (200, 200, 200), 
+                        FONT_THICKNESS)
+        else:
+            print("No match found")
+
+    # cv2.imshow(filename, image)
+    print()
+    print()
+    cv2.waitKey(10000)
+    # cv2.destroyWindow(filename)   <-- doesn't work on Ubuntu
